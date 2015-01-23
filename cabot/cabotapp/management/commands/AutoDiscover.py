097099cf31165e732b92a0e9e16c805e59f86c68
Andreu B <andreu.belmonte@gmail.com>
2015-01-23 14:06:28 +0100

    AutoDiscover update

--- cabot/cabotapp/management/commands/AutoDiscover.py
+++ cabot/cabotapp/management/commands/AutoDiscover.py
@@ -10,19 +10,28 @@ from cabot.cabotapp.models import *
 from cabot.cabotapp.graphite import *
 import re
 
-IGNORED_MOUNTS =(
+IGNORED_MOUNTS = [
   "df-run",
   "df-dev"
-)
+]
+
+IGNORED_HOSTS = [
+  "staging.ofertia.com",
+  "testing.ofertia.com"
+]
 
 def isValidHost(name):
+  if name.replace("_",".") in IGNORED_HOSTS: return False
   regexp=re.compile(".*(_\w)+")
   return regexp.match(name)
+
+def hasOwnDomain(instance):
+  return instance.find("-") == -1
   
 def FormatMountPoinName(name):
   return "/"+name[3:].replace("-","/")
 
-def AddServiceCheck(instance, name, metric, check_type, value, debounce=0):
+def AddServiceCheck(service, instance, name, metric, check_type, value, debounce=0, frequency=1, expected_num_hosts=1):
   """
   Create check for metric, and add to service
   """
@@ -32,10 +41,14 @@ def AddServiceCheck(instance, name, metric, check_type, value, debounce=0):
       defaults = {
         "check_type": check_type,
         "value": value,
-        "created_by_id": 2,  # hard coded to user 1
+        "created_by_id": 2,
         "importance": Service.ERROR_STATUS,
+        "frequency": frequency,
+        "debounce": debounce,
+        "expected_num_hosts": expected_num_hosts,
         }
   )
+  service.status_checks.add(check);
   instance.status_checks.add(check);
 
 class Command(BaseCommand):
@@ -48,6 +61,12 @@ class Command(BaseCommand):
       if not isValidHost(metric["name"]): continue
       server, domain  = metric["name"].split("_",1)
       domain = domain.replace("_",".")
+      address = ".".join((server,domain))
+      service_url = "www."+domain
+      if hasOwnDomain(server):
+        domain = ".".join((server,domain))
+        address = domain
+        service_url = domain
       server_path = metric["path"]
       print server, domain, server_path
 
@@ -56,7 +75,7 @@ class Command(BaseCommand):
       service.users_to_notify.add(user)
 
       # get or create instance
-      instance, instance_created = Instance.objects.get_or_create(name=server, defaults={"address": ".".join((server,domain)), "email_alert": True, "hipchat_alert": False, })
+      instance, instance_created = Instance.objects.get_or_create(name=server, defaults={"address": address, "email_alert": True, "hipchat_alert": False, })
       instance.users_to_notify.add(user)
       service.instances.add(instance)
 
@@ -66,19 +85,26 @@ class Command(BaseCommand):
         name, metric = FormatMountPoinName(disk_metric["name"]), disk_metric["path"]
         if any(v in name for v in IGNORED_MOUNTS): continue
         print name,metric
-        AddServiceCheck(instance, 
+        AddServiceCheck(service, instance, 
           "%s disk %s" % (server, name), 
           "asPercent(%sfree,sumSeries(%s*))" % (metric, metric), 
           "<", 
           "15.0");
 
-      # Add check for load over 5
+      # Add check for load over
       num_cpu = float(len(get_matching_metrics("%scpu-*" % (server_path))["metrics"]))
-      AddServiceCheck(instance, 
+      AddServiceCheck(service, instance, 
         "%s load" % (server, ), 
         "%sload.load.midterm" % (server_path, ), 
         ">", 
         num_cpu);
 
-      # Add check for low swap+free  memory
-      #AddServiceCheck(service, "%s low mem" % (server, ), "sumSeries(%smemory.SwapFree, %smemory.MemFree)"% (server_path, server_path), "<", "1000000");
+      # Add check for memory
+      AddServiceCheck(service, instance, 
+        "%s low mem" % (server, ), 
+        "asPercent(sumSeries(%smemory.{memory-cached,memory-free}),sumSeries(%smemory.*))"% (server_path, server_path),
+        "<", 
+        "10",
+        frequency=5,
+        debounce=6,
+      );
